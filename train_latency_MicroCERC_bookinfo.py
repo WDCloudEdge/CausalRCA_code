0c652365a8f5c2ec7b1145739b3e03ad0d9e31b4
zhuyuhan2333 <zhuyuhan2333@qq.com>
2024-02-09 21:34:11 +0800

    feat: support pc with microcerc dataset bookinfo ratings-edge

--- train_latency_MicroCERC_bookinfo.py
+++ train_latency_MicroCERC_bookinfo.py
@@ -170,8 +170,8 @@ def train(epoch, best_val_loss, lambda_A, c_A, optimizer):
 # main
 # ===================================
 if __name__ == '__main__':
-    # pps = ['p50', 'p90', 'p99']
-    pps = ['p50']
+    pps = ['p50', 'p90', 'p99']
+    # pps = ['p50']
     simples: List[Simple] = [
         Simple(
             1706322060, 1706322840, 'label-ratings-edge-net-latency-1', 'ratings-v1-edge',
@@ -250,178 +250,26 @@ if __name__ == '__main__':
             name = [i for i in all_data.columns if i != 'timestamp' and pp in i]
             data = all_data[name]
 
-            data_sample_size = data.shape[0]
-            data_variable_size = data.shape[1]
-            # torch.manual_seed(CONFIG.seed)
-            # if CONFIG.cuda:
-            #     torch.cuda.manual_seed(CONFIG.seed)
+            from causallearn.search.ConstraintBased.PC import pc
+            from causallearn.utils.cit import chisq
 
-            # ================================================
-            # get data: experiments = {synthetic SEM, ALARM}
-            # ================================================
-            train_data = data
+            cg = pc(data.to_numpy(), 0.05, chisq, False, 0, -1)
+            adj = cg.G.graph
 
-            # ===================================
-            # load modules
-            # ===================================
-            # Generate off-diagonal interaction graph
-            off_diag = np.ones([data_variable_size, data_variable_size]) - np.eye(data_variable_size)
+            # print('PC result')
+            # print(adj)
 
-            # add adjacency matrix A
-            num_nodes = data_variable_size
-            adj_A = np.zeros((num_nodes, num_nodes))
-
-            if CONFIG.encoder == 'mlp':
-                encoder = MLPEncoder(data_variable_size * CONFIG.x_dims, CONFIG.x_dims, CONFIG.encoder_hidden,
-                                     int(CONFIG.z_dims), adj_A,
-                                     batch_size=CONFIG.batch_size,
-                                     do_prob=CONFIG.encoder_dropout, factor=CONFIG.factor).double()
-            elif CONFIG.encoder == 'sem':
-                encoder = SEMEncoder(data_variable_size * CONFIG.x_dims, CONFIG.encoder_hidden,
-                                     int(CONFIG.z_dims), adj_A,
-                                     batch_size=CONFIG.batch_size,
-                                     do_prob=CONFIG.encoder_dropout, factor=CONFIG.factor).double()
-
-            if CONFIG.decoder == 'mlp':
-                decoder = MLPDecoder(data_variable_size * CONFIG.x_dims,
-                                     CONFIG.z_dims, CONFIG.x_dims, encoder,
-                                     data_variable_size=data_variable_size,
-                                     batch_size=CONFIG.batch_size,
-                                     n_hid=CONFIG.decoder_hidden,
-                                     do_prob=CONFIG.decoder_dropout).double()
-            elif CONFIG.decoder == 'sem':
-                decoder = SEMDecoder(data_variable_size * CONFIG.x_dims,
-                                     CONFIG.z_dims, 2, encoder,
-                                     data_variable_size=data_variable_size,
-                                     batch_size=CONFIG.batch_size,
-                                     n_hid=CONFIG.decoder_hidden,
-                                     do_prob=CONFIG.decoder_dropout).double()
-
-            # ===================================
-            # set up training parameters
-            # ===================================
-            if CONFIG.optimizer == 'Adam':
-                optimizer = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=CONFIG.lr)
-            elif CONFIG.optimizer == 'LBFGS':
-                optimizer = optim.LBFGS(list(encoder.parameters()) + list(decoder.parameters()),
-                                        lr=CONFIG.lr)
-            elif CONFIG.optimizer == 'SGD':
-                optimizer = optim.SGD(list(encoder.parameters()) + list(decoder.parameters()),
-                                      lr=CONFIG.lr)
-
-            scheduler = lr_scheduler.StepLR(optimizer, step_size=CONFIG.lr_decay,
-                                            gamma=CONFIG.gamma)
-
-            # Linear indices of an upper triangular mx, used for acc calculation
-            triu_indices = get_triu_offdiag_indices(data_variable_size)
-            tril_indices = get_tril_offdiag_indices(data_variable_size)
-
-            if CONFIG.prior:
-                prior = np.array([0.91, 0.03, 0.03, 0.03])  # hard coded for now
-                print("Using prior")
-                print(prior)
-                log_prior = torch.DoubleTensor(np.log(prior))
-                log_prior = torch.unsqueeze(log_prior, 0)
-                log_prior = torch.unsqueeze(log_prior, 0)
-                log_prior = Variable(log_prior)
-
-                if CONFIG.cuda:
-                    log_prior = log_prior.cuda()
-
-            if CONFIG.cuda:
-                encoder.cuda()
-                decoder.cuda()
-                triu_indices = triu_indices.cuda()
-                tril_indices = tril_indices.cuda()
-            gamma = args.gamma
-            eta = args.eta
-
-            t_total = time.time()
-            best_ELBO_loss = np.inf
-            best_NLL_loss = np.inf
-            best_MSE_loss = np.inf
-            best_epoch = 0
-            best_ELBO_graph = []
-            best_NLL_graph = []
-            best_MSE_graph = []
-            # optimizer step on hyparameters
-            c_A = CONFIG.c_A
-            lambda_A = CONFIG.lambda_A
-            h_A_new = torch.tensor(1.)
-            h_tol = CONFIG.h_tol
-            k_max_iter = int(CONFIG.k_max_iter)
-            h_A_old = np.inf
-
-            E_loss = []
-            N_loss = []
-            M_loss = []
-            start_time = time.time()
-            try:
-                for step_k in range(k_max_iter):
-                    # print(step_k)
-                    while c_A < 1e+20:
-                        for epoch in range(CONFIG.epochs):
-                            # print(epoch)
-                            ELBO_loss, NLL_loss, MSE_loss, graph, origin_A = train(epoch, best_ELBO_loss, lambda_A, c_A,
-                                                                                   optimizer)
-                            E_loss.append(ELBO_loss)
-                            N_loss.append(NLL_loss)
-                            M_loss.append(MSE_loss)
-                            if ELBO_loss < best_ELBO_loss:
-                                best_ELBO_loss = ELBO_loss
-                                best_epoch = epoch
-                                best_ELBO_graph = graph
-
-                            if NLL_loss < best_NLL_loss:
-                                best_NLL_loss = NLL_loss
-                                best_epoch = epoch
-                                best_NLL_graph = graph
-
-                            if MSE_loss < best_MSE_loss:
-                                best_MSE_loss = MSE_loss
-                                best_epoch = epoch
-                                best_MSE_graph = graph
-
-                        # print("Optimization Finished!")
-                        # print("Best Epoch: {:04d}".format(best_epoch))
-                        if ELBO_loss > 2 * best_ELBO_loss:
-                            break
-
-                        # update parameters
-                        A_new = origin_A.data.clone()
-                        h_A_new = _h_A(A_new, data_variable_size)
-                        if h_A_new.item() > gamma * h_A_old:
-                            c_A *= eta
-                        else:
-                            break
-
-                    # update parameters
-                    # h_A, adj_A are computed in loss anyway, so no need to store
-                    h_A_old = h_A_new.item()
-                    lambda_A += c_A * h_A_new.item()
-
-                    if h_A_new.item() <= h_tol:
-                        break
-
-                # print("Steps: {:04d}".format(step_k))
-                # print("Best Epoch: {:04d}".format(best_epoch))
-
-                # test()
-                # print (best_ELBO_graph)
-                # print(best_NLL_graph)
-                # print (best_MSE_graph)
-
-                graph = origin_A.data.clone().cpu().numpy()
-                graph[np.abs(graph) < 0.1] = 0
-                graph[np.abs(graph) < 0.2] = 0
-                graph[np.abs(graph) < 0.3] = 0
-
-            except KeyboardInterrupt:
-                print('Done!')
-
-            end_time = time.time()
-            # print("Time spent: ",end_time-start_time)
-            adj = graph
+            # Change the adj to graph
+            G = nx.DiGraph()
+            for i in range(len(adj)):
+                for j in range(len(adj)):
+                    if adj[i, j] == -1:
+                        G.add_edge(i, j)
+                    if adj[i, j] == 1:
+                        G.add_edge(j, i)
+            nodes = sorted(G.nodes())
+            # print(nodes)
+            adj = np.asarray(nx.to_numpy_matrix(G, nodelist=nodes))
             if not np.any(adj):
                 print(simple.label + ' is absent')
                 continue
